e67a36a714b8816c40280ff488f7c8f2ee1fae37
MinalAmbatkar15 <68222272+MinalAmbatkar15@users.noreply.github.com>
2020-07-17 14:00:50 +0530

    Add files via upload

---
+++ assignment3.1.js
@@ -0,0 +1,17 @@
+<html>
+<title>Even Odd Program</title>
+<body>
+<script>
+</script>
+document.write("Enter the value");
+isEven();
+
+function isEven(value) {
+	if (value%2 == 0)
+		console.log("The number entered is"+value+"and Number is Even");
+	else
+		console.log("The number entered is"+value+"and Number is Odd");
+}
+
+</body>
+</html>
